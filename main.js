cad6b5a145305cc3f61d3f6c227a11b4a1217b61
reginaldo melo <reginaldo.melo56@gmail.com>
2020-09-24 12:24:46 -0300

    1

---
+++ main.js
@@ -0,0 +1,9 @@
+function teste (){
+    var d = new Date();
+    d.setTime(d.getTime() + (24*60*60*1000));
+   var expires = "expires="+ d.toUTCString();
+   document.cookie = 'visit' + "=" + 'cvalue' + ";" + expires + ";path=/";
+
+   fetch('http://localhost:3005').then((res)=>res.json())
+    return  document.cookie;  
+}  
